4da179e0d9e5218b0418925ee59dcf18348cb2bc
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-01 03:34:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,10 @@ def turn)count(board)
   squares_filled_in=9 
   ii=0 
   while(ii<9)
-    
+    if board[ii]== " "
+      squares_filled_in -=1 
+    end 
+    ii+=1
   end
+  return squares_filled_in
 end
